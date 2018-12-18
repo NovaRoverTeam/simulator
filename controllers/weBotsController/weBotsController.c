1051a60a32ebe5550a821faa88c10dac61273c2f
Liam Stow <liam.stow@outlook.com>
2018-12-18 23:46:31 +1100

    added middle right wheel with motor

--- controllers/weBotsController/weBotsController.c
+++ controllers/weBotsController/weBotsController.c
@@ -8,7 +8,9 @@ int main() {
 
   // initialize motors
   WbDeviceTag frontRight = wb_robot_get_device("rtm_front_right_wheel");
-  WbDeviceTag backRight = wb_robot_get_device("rtm_back_right_wheel");
+  WbDeviceTag backRight = wb_robot_get_device("rtm_back_right_wheel"); 
+  WbDeviceTag midRight = wb_robot_get_device("rtm_mid_right_wheel");
+  
   WbDeviceTag frontLeft = wb_robot_get_device("rtm_front_left_wheel");
   WbDeviceTag backLeft = wb_robot_get_device("rtm_back_left_wheel");
 
@@ -18,6 +20,10 @@ int main() {
   wb_motor_set_velocity(frontRight, speed);
   wb_motor_set_position(backRight, INFINITY);
   wb_motor_set_velocity(backRight, speed);
+  wb_motor_set_position(midRight, INFINITY);
+  wb_motor_set_velocity(midRight, speed);
+  
+  
   wb_motor_set_position(frontLeft, INFINITY);
   wb_motor_set_velocity(frontLeft, speed);
   wb_motor_set_position(backLeft, INFINITY);
