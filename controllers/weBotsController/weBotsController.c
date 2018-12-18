3e09a9332c824392ff6a05cb99a7726ac66cd2d9
Liam Stow <liam.stow@outlook.com>
2018-12-18 22:44:44 +1100

    added front and back wheels with motors

---
+++ controllers/weBotsController/weBotsController.c
@@ -0,0 +1,31 @@
+#include <webots/robot.h>
+#include <webots/motor.h>
+
+#define TIME_STEP 64
+
+int main() {
+   wb_robot_init();
+
+  // initialize motors
+  WbDeviceTag frontRight = wb_robot_get_device("rtm_front_right_wheel");
+  WbDeviceTag backRight = wb_robot_get_device("rtm_back_right_wheel");
+  WbDeviceTag frontLeft = wb_robot_get_device("rtm_front_left_wheel");
+  WbDeviceTag backLeft = wb_robot_get_device("rtm_back_left_wheel");
+
+  //Set speeds
+  double speed = 1.5;
+  wb_motor_set_position(frontRight, INFINITY);
+  wb_motor_set_velocity(frontRight, speed);
+  wb_motor_set_position(backRight, INFINITY);
+  wb_motor_set_velocity(backRight, speed);
+  wb_motor_set_position(frontLeft, INFINITY);
+  wb_motor_set_velocity(frontLeft, speed);
+  wb_motor_set_position(backLeft, INFINITY);
+  wb_motor_set_velocity(backLeft, speed);
+  
+  while (wb_robot_step(TIME_STEP) != -1){
+  }
+  
+  wb_robot_cleanup();
+  return 0;
+}
